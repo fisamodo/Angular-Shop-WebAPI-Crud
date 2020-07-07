bd74fdd91b27b2882281ce96defa6f54b9744543
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-07-07 15:30:45 +0200

    popup dialog for order

--- src/app/orders/order-items/order-items.component.ts
+++ src/app/orders/order-items/order-items.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { OrderItem } from 'src/app/shared/order-item.model';
 
 @Component({
   selector: 'app-order-items',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: []
 })
 export class OrderItemsComponent implements OnInit {
+  formData: OrderItem;
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data,
+    public dialogRef: MatDialogRef<OrderItemsComponent>
+  ) { }
 
   ngOnInit(): void {
+    this.formData={
+      NaruceniProizvodID: null,
+      NarudzbaID: this.data.NarudzbaID,
+      ProizvodID: 0,
+      Proizvod:'',
+      Cijena: 0,
+      Kolicina:0,
+      Ukupno:0
+    }
   }
 
 }
