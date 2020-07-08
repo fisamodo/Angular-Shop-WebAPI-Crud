0af10369b65c9572a3f92f0a19d58dcfc7358766
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-07-08 12:53:31 +0200

    Consumed proizvod API

--- src/app/orders/order-items/order-items.component.ts
+++ src/app/orders/order-items/order-items.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { OrderItem } from 'src/app/shared/order-item.model';
+import { ItemService } from 'src/app/shared/item.service';
+import { Item } from 'src/app/shared/item.model';
 
 @Component({
   selector: 'app-order-items',
@@ -9,13 +11,17 @@ import { OrderItem } from 'src/app/shared/order-item.model';
 })
 export class OrderItemsComponent implements OnInit {
   formData: OrderItem;
+  itemList: Item[];
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data,
-    public dialogRef: MatDialogRef<OrderItemsComponent>
+    public dialogRef: MatDialogRef<OrderItemsComponent>,
+    private itemService: ItemService
   ) { }
 
   ngOnInit(): void {
+    this.itemService.getItemList().then(res => this.itemList = res as Item[]);
+
     this.formData={
       NaruceniProizvodID: null,
       NarudzbaID: this.data.NarudzbaID,
--- src/app/shared/item.service.ts
+++ src/app/shared/item.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getItemList(){
+    return this.http.get(environment.apiURL+'/Proizvod').toPromise();
+  }
 }
