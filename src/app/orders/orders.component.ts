b1e46a4d544ec58c3d2155d37b3ec102fdc0ed10
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-07-13 02:49:40 +0200

    added delete, fixed post/put method and works

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from '../shared/order.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-orders',
@@ -11,14 +13,26 @@ export class OrdersComponent implements OnInit {
   orderList;
 
   constructor(private service:OrderService,
-    private router: Router) { }
+    private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.service.getOrderList().then(res => this.orderList = res);
+  }
+  refreshList() {
     this.service.getOrderList().then(res => this.orderList = res);
   }
 
   openForEdit(orderID: number){
     this.router.navigate(['/order/edit/' + orderID]);
+  }
+  onOrderDelete(id: number) {
+    if (confirm('Potvrdite otkazivanje rezervacije!')) {
+      this.service.deleteOrder(id).then(res => {
+        this.refreshList();
+        this.toastr.warning("Uspješno Otkazano", "Filip Car Parts Shop.");
+      });
+    }
   }
 
 }
--- src/app/shared/item.service.ts
+++ src/app/shared/item.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +10,6 @@ export class ItemService {
   constructor(private http:HttpClient) { }
 
   getItemList(){
-    return this.http.get(environment.apiURL+'/item').toPromise();
+   return this.http.get(environment.apiURL+'/Item').toPromise();
   }
 }
