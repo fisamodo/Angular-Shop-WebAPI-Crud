23086d419adb9e52de8e15d5d272357d0fbff24f
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-07-13 14:36:02 +0200

    updated database model

--- src/app/shared/customer.model.ts
+++ src/app/shared/customer.model.ts
@@ -1,4 +1,5 @@
 export class Customer {
     CustomerID :number;
     Name :string;
+    Email: string;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL: 'https://localhost:44395/api'
+  apiURL: 'https://localhost:44370/api'
 };
 
 /*
