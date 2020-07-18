f34965b2f39648120627dea155d99b5b4a836a84
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-07-18 11:41:06 +0200

    added new azure url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL: 'https://apiwithemail20200716052243.azurewebsites.net/api'
+  apiURL: 'https://webapiazurezavrsni.azurewebsites.net/api'
 };
 
 /*
